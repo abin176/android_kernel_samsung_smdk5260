e2e922caa263cfa7ec350df28f491ce261e62f08
FireLord1 <firelord.xda@gmail.com>
2016-11-12 14:50:18 +0530

    Revert "inetpeer: get rid of ip_id_count"
    This reverts commit ad52eef552c7896ec6024ee72fc126167fe5c4e2.
    
    Signed-off-by: FireLord1 <firelord.xda@gmail.com>

--- net/ipv4/route.c
+++ net/ipv4/route.c
@@ -1382,7 +1382,6 @@ static void ip_select_fb_ident(struct iphdr *iph)
 	static DEFINE_SPINLOCK(ip_fb_id_lock);
 	static u32 ip_fallback_id;
 	u32 salt;
-
 	spin_lock_bh(&ip_fb_id_lock);
 	salt = secure_ip_id((__force __be32)ip_fallback_id ^ iph->daddr);
 	iph->id = htons(salt & 0xFFFF);
